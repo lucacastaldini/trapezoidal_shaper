6a04ed065280d50b356451e4fa5a552d8ed0953b
Luca Castaldini <luca.castaldini@cta-consortium.org>
2024-09-27 18:40:25 +0200

    save timinig and new trap method

--- traditional_algorithms/impl_exp.py
+++ traditional_algorithms/impl_exp.py
@@ -1,13 +1,17 @@
 
-from tps import trap_filter, exp_func
+from tps import compute_and_separate_trap, plot_traps, trap_filter, exp_func
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import find_peaks, find_peaks_cwt
 
 # Esempio di utilizzo della funzione
 dd = 1
 N = 1000
 H1=1
+H2=2
 M = 600
+
+sim_bias=1
 
 # Array dei campioni
 nn = np.arange(0, N)
@@ -15,37 +19,18 @@ tt = nn * dd
 
 # Array della forma d'onda di input
 t1 = 10 * dd
-t2 = 4000 * dd
+t2 = 100 * dd
 v1 = exp_func(tt, H1, t1, M)
-v2 = exp_func(tt, 0, t2, M)
+v2 = exp_func(tt, H2, t2, M)
 vv = v1 + v2
 
+
 # Parametri del filtro trapezoidale
-m = 200
-l = 100
+m = 50
+l = 20
 
 # Chiamata alla funzione
 dml_vals, p_vals, s_vals , s_vals_scaled = trap_filter(M, dd, vv, m, l)
 
-# Plot s_vals and vv against tt
-plt.figure(figsize=(10, 6))
 
-# Plot s_vals
-plt.plot(tt, s_vals_scaled, label='s_vals', color='b', linewidth=2)
-
-# Plot vv
-plt.plot(tt, vv, label='vv', color='r', linewidth=2)
-
-# Add labels and title
-plt.xlabel('Time (s)')
-plt.ylabel('Amplitude')
-plt.title('s_vals and vv vs. Time')
-
-# Add legend
-plt.legend()
-
-# Display grid
-plt.grid(True)
-
-# Show the plot
-plt.show()
+plot_traps(tt=tt, input=vv, out_scaled=[s_vals_scaled,], peaks_signal=dml_vals, t_zeros=None, t_end=None)
